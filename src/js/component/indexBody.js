7a5edc28dea5e5d9850a4e850f7a5fa7b95ace24
hhzh <1138253815@qq.com>
2017-10-21 16:52:20 +0800

    update

---
+++ src/js/component/indexBody.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class ComponentBody extends React.Component {
+    render() {
+        return (
+            <header>
+                <h1>页面的主题内容</h1>
+            </header>
+        )
+    }
+}
