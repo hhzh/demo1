8b127f70c4d442dcd9e09d4c0f2b249f138eb326
hhzh <1138253815@qq.com>
2017-10-21 17:06:19 +0800

    commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 import ComponentHeader from './component/header'
+import ComponentFooter from './component/footer'
+import ComponentBody from './component/indexBody'
 
 // ReactDOM.render(
 //     <h1>Hello World.</h1>,
@@ -10,7 +12,11 @@ import ComponentHeader from './component/header'
 class Index extends React.Component {
     render() {
         return (
-            <ComponentHeader/>
+            <div>
+                <ComponentHeader/>
+                <ComponentBody/>
+                <ComponentFooter/>
+            </div>
         )
     }
 }
