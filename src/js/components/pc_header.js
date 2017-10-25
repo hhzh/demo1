adda581172ce444a575d455d68f92e1729e05fd8
hhzh <1138253815@qq.com>
2017-10-25 18:09:01 +0800

    off duty

--- src/js/components/pc_header.js
+++ src/js/components/pc_header.js
@@ -1,20 +1,70 @@
 import React from 'react';
-import {Row, Col} from 'antd';
-import {Menu, Icon} from 'antd';
+import {Menu, Icon, Row, Col, Tabs, message, Form, Input, Button, Checkbox, Modal} from 'antd';
 
+const FormItem = Form.Item;
 const SubMenu = Menu.SubMenu;
+const TabPane = Tabs.TabPane;
 const MenuItemGroup = Menu.ItemGroup;
 
-export default class PCHeader extends React.Component {
+class PCHeader extends React.Component {
 
-    constructor(){
+    constructor() {
         super();
         this.state = {
-            current: 'top'
+            current: 'top',
+            modalVisible: false,
+            action: 'login',
+            hasLogined: false,
+            userNickName: '',
+            userid: 0
         };
-    }
+    };
+
+    setModalVisible(value) {
+        this.setState({modalVisible: value})
+    };
+
+    handleClick(e) {
+        if (e.key = "register") {
+            this.setState({current: "register"});
+            this.setModalVisible(true);
+        } else {
+            this.setState({current: e.key})
+        }
+    };
+
+    handleSubmit(e) {
+        e.preventDefault();
+        var myFetchOptions = {
+            method: 'GET'
+        };
+        var formData = this.props.form.getFieldsValue();
+        console.log(formData);
+        fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=register&username=userName&password=password&r_userName="
+            + formData.r_userName
+            + "&r_password=" + formData.r_password + "&r_confirmPassword=" + formData.r_confirmPassword, myFetchOptions)
+            .then(response => response.json()).then(json => {
+            this.setState({userNickName: json.NickUserName, userid: json.UserId});
+        });
+        message.success("请求成功！");
+        this.setModalVisible(false);
+    };
 
     render() {
+        let {getFieldProps} = this.props.form;
+        const userShow = this.state.hasLogined
+            ? <Menu.Item key="logout" class="register">
+                <Button type="primary" htmlType="button">{this.state.userNickName}</Button>
+                &nbsp;&nbsp;
+                <Link target="_blank">
+                    <Button type="dashed" htmlType="button">个人中心</Button>
+                </Link>
+                &nbsp;&nbsp;
+                <Button type="ghost" htmlType="button">退出</Button>
+            </Menu.Item>
+            : <Menu.Item key="register" class="register">
+                <Icon type="appstore"/>注册/登录
+            </Menu.Item>;
         return (
             <header>
                 <Row>
@@ -26,7 +76,7 @@ export default class PCHeader extends React.Component {
                         </a>
                     </Col>
                     <Col span={16}>
-                        <Menu mode="horizontal" selectKeys={[this.state.current]}>
+                        <Menu mode="horizontal" selectKeys={[this.state.current]} onClick={this.handleClick.bind(this)}>
                             <Menu.Item key="top">
                                 <Icon type="appstore"/>头条
                             </Menu.Item>
@@ -51,10 +101,32 @@ export default class PCHeader extends React.Component {
                             <Menu.Item key="shishang">
                                 <Icon type="appstore"/>时尚
                             </Menu.Item>
+                            {userShow}
                         </Menu>
+                        <Modal title="用户中心" wrapClassName="vertical-center-modal" visible={this.state.modalVisible}
+                               onCancel={() => this.setModalVisible(false)}
+                               onOk={() => this.setModalVisible(false)} onText="关闭">
+                            <Tabs type="card">
+                                <TabPane tab="注册" key="2">
+                                    <Form horizontal onSubmit={this.handleSubmit.bind(this)}>
+                                        <FormItem label="账户">
+                                            <Input placeholder="请输入你的账号" {...getFieldProps('r_userName')}/>
+                                        </FormItem>
+                                        <FormItem label="密码">
+                                            <Input type="password"
+                                                   placeholder="请再次输入你的密码" {...getFieldProps('r_confirmPassword')}/>
+                                        </FormItem>
+                                        <Button type="primary" htmlType="submit">注册</Button>
+                                    </Form>
+                                </TabPane>
+                            </Tabs>
+                        </Modal>
+                        <Col span={2}/>
                     </Col>
                 </Row>
             </header>
         );
     };
 }
+
+export default PCHeader = Form.create({})(PCHeader);
