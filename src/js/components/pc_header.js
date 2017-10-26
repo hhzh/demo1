3e6828df0fe7827c46e145e8caa77a208cad9858
hhzh <1138253815@qq.com>
2017-10-26 18:47:51 +0800

    add

--- src/js/components/pc_header.js
+++ src/js/components/pc_header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Menu, Icon, Row, Col, Tabs, message, Form, Input, Button, Checkbox, Modal} from 'antd';
+import {Router, Route, Link, browserHistory} from 'react-router';
 
 const FormItem = Form.Item;
 const SubMenu = Menu.SubMenu;
@@ -21,7 +22,7 @@ class PCHeader extends React.Component {
     };
 
     setModalVisible(value) {
-        this.setState({modalVisible: value})
+        this.setState({modalVisible: value});
     };
 
     handleClick(e) {
@@ -40,14 +41,29 @@ class PCHeader extends React.Component {
         };
         var formData = this.props.form.getFieldsValue();
         console.log(formData);
-        fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=register&username=userName&password=password&r_userName="
-            + formData.r_userName
+        fetch("http://newsapi.gugujiankong.com/Handler.ashx?action="+this.state.action+"&username="+this.state.username+
+        "&password="+this.state.password+"&r_userName=+" + formData.r_userName
             + "&r_password=" + formData.r_password + "&r_confirmPassword=" + formData.r_confirmPassword, myFetchOptions)
             .then(response => response.json()).then(json => {
             this.setState({userNickName: json.NickUserName, userid: json.UserId});
         });
+        if (this.state.action=="login") {
+            this.setState({hasLogined:true});
+        }
         message.success("请求成功！");
         this.setModalVisible(false);
+    };
+
+    login(){
+        this.setModalVisible(true);
+    };
+
+    callback(key) {
+        if (key == 1) {
+            this.setState({action: 'login'});
+        } else if (key == 2) {
+            this.setState({action: 'register'});
+        }
     };
 
     render() {
@@ -72,7 +88,7 @@ class PCHeader extends React.Component {
                     <Col span={4}>
                         <a href="/" class="logo">
                             <img src="./src/images/logo.png" alt="logo"></img>
-                            <span>ReactNews</span>
+                            <span>ReactNews..</span>
                         </a>
                     </Col>
                     <Col span={16}>
@@ -106,7 +122,18 @@ class PCHeader extends React.Component {
                         <Modal title="用户中心" wrapClassName="vertical-center-modal" visible={this.state.modalVisible}
                                onCancel={() => this.setModalVisible(false)}
                                onOk={() => this.setModalVisible(false)} onText="关闭">
-                            <Tabs type="card">
+                            <Tabs type="card" onChange={this.callback.bind(this)}>
+                                <TabPane tab="登录" key="1">
+                                    <Form horizontal onSubmit={this.handleSubmit.bind(this)}>
+                                        <FormItem label="账户">
+                                            <Input placeholder="请输入您的账号" {...getFieldDecorator('userName')}/>
+                                        </FormItem>
+                                        <FormItem label="密码">
+                                            <Input type="password" placeholder="请输入您的密码" {...getFieldDecorator('password')}/>
+                                        </FormItem>
+                                        <Button type="primary" htmlType="submit">登录</Button>
+                                    </Form>
+                                </TabPane>
                                 <TabPane tab="注册" key="2">
                                     <Form horizontal onSubmit={this.handleSubmit.bind(this)}>
                                         <FormItem label="账户">
